4adbb0609f008a7fa5f2dcb3ea1e0a8867b7ee29
Jeffrey Chen <j.chen14641@gmail.com>
2020-06-11 22:19:46 -0700

    Initial Commit

---
+++ Drinktracker_MenuBar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Drinktracker_MenuBar
+//
+//  Created by Jeffrey Chen on 6/11/20.
+//  Copyright © 2020 Jeffrey Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
